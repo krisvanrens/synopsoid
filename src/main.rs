8cc50313958589130dc2ab8b15d05cd596731283
Kris van Rens <krisvanrens@gmail.com>
2023-02-06 21:44:18 +0100

    RFT: Refactor functions.

--- src/main.rs
+++ src/main.rs
@@ -22,26 +22,34 @@ fn main() {
     parse_lines(args.path);
 }
 
-fn parse_line(line: &str) {
-    lazy_static! {
-        static ref RE_HEADER: Regex = Regex::new(r"^#+.*$").unwrap();
-        static ref RE_H1: Regex = Regex::new(r"^#\s.*$").unwrap();
-        static ref RE_H2: Regex = Regex::new(r"^##\s.*$").unwrap();
-    }
-
-    if RE_HEADER.is_match(line) {
-        if RE_H1.is_match(line) {
-            println!("H1: '{line}'");
-        } else if RE_H2.is_match(line) {
-            println!("H2: '{line}'");
-        }
-    }
-}
-
 fn parse_lines<P>(filename: P)
 where
     P: AsRef<Path>,
 {
+    fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+    where
+        P: AsRef<Path>,
+    {
+        let file = File::open(filename)?;
+        Ok(io::BufReader::new(file).lines())
+    }
+
+    fn parse_line(line: &str) {
+        lazy_static! {
+            static ref RE_HEADER: Regex = Regex::new(r"^#+.*$").unwrap();
+            static ref RE_H1: Regex = Regex::new(r"^#\s.*$").unwrap();
+            static ref RE_H2: Regex = Regex::new(r"^##\s.*$").unwrap();
+        }
+
+        if RE_HEADER.is_match(line) {
+            if RE_H1.is_match(line) {
+                println!("H1: '{line}'");
+            } else if RE_H2.is_match(line) {
+                println!("H2: '{line}'");
+            }
+        }
+    }
+
     if let Ok(line_buffer) = read_lines(&filename) {
         for line in line_buffer {
             if let Ok(l) = line {
@@ -53,12 +61,4 @@ where
     } else {
         eprintln!("Failed to open file '{}'", filename.as_ref().display());
     }
-}
-
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
 }
